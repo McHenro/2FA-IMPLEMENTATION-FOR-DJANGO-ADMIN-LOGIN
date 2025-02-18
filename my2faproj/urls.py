5aee83b4c80e8e4ef598b13f0d2ac346139f1888
Henry Anorue <hanorue@drohealth.com>
2025-02-18 08:02:12 +0100

    Configure the project Urls and the Project settings

--- my2faproj/urls.py
+++ my2faproj/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Standard admin interface with two-factor enforced
+    path("",include(("authentication.urls", "authentication"), namespace="authentication")),
 ]
